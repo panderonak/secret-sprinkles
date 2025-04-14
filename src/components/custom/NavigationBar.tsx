3502cb16533d9eb355114152da961c4f4b9a1160
panderonak <pandeyronak139@gmail.com>
2025-04-14 18:39:24 +0530

    feat(navbar): add NavigationBar component with auth-aware buttons using NextAuth.

---
+++ src/components/custom/NavigationBar.tsx
@@ -0,0 +1,42 @@
+"use client";
+
+import { Navbar, NavbarBrand, NavbarContent, NavbarItem } from "@heroui/navbar";
+import Link from "next/link";
+import { Button } from "../ui/button";
+import { useSession, signOut } from "next-auth/react";
+import { User } from "next-auth";
+
+export default function NavigationBar() {
+  const { data: session, status } = useSession();
+
+  console.log(`Nav bar: ${status}`);
+
+  const user: User = session?.user as User;
+
+  return (
+    <>
+      <div className="navbar shadow-sm py-5 px-6">
+        <Navbar>
+          <div className="flex-1">
+            <NavbarBrand>
+              <p className="font-bold text-lg text-inherit">Secret Sprinkles</p>
+            </NavbarBrand>
+          </div>
+          <div className="flex-none">
+            <NavbarContent justify="end">
+              <NavbarItem>
+                {session ? (
+                  <Button onClick={() => signOut()}>Sign Out</Button>
+                ) : (
+                  <Link href="/sign-in">
+                    <Button>Sign In</Button>
+                  </Link>
+                )}
+              </NavbarItem>
+            </NavbarContent>
+          </div>
+        </Navbar>
+      </div>
+    </>
+  );
+}
