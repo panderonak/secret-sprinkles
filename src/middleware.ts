dd36f3cf3e8e4c5126e1d1371549ef2031c8930c
panderonak <pandeyronak139@gmail.com>
2025-04-20 20:55:59 +0530

    fix(middleware): correct auth redirect logic and matcher pattern.

--- src/middleware.ts
+++ src/middleware.ts
@@ -9,11 +9,11 @@ export async function middleware(request: NextRequest) {
   const url = request.nextUrl;
 
   if (
-    (token &&
-      (url.pathname.startsWith("/sign-in") ||
-        url.pathname.startsWith("/sign-up") ||
-        url.pathname.startsWith("/verify"))) ||
-    url.pathname === "/"
+    token &&
+    (url.pathname.startsWith("/sign-in") ||
+      url.pathname.startsWith("/sign-up") ||
+      url.pathname.startsWith("/verify") ||
+      url.pathname === "/")
   ) {
     return NextResponse.redirect(new URL("/dashboard", request.url));
   }
@@ -21,9 +21,11 @@ export async function middleware(request: NextRequest) {
   if (!token && url.pathname.startsWith("/dashboard")) {
     return NextResponse.redirect(new URL("/sign-in", request.url));
   }
+
+  return NextResponse.next();
 }
 
 // See "Matching Paths" below to learn more
 export const config = {
-  matcher: ["/sign-in", "/sign-up", "/", "/dashboard/:path*", "/verify:path*"],
+  matcher: ["/sign-in", "/sign-up", "/", "/dashboard/:path*", "/verify/:path*"],
 };
